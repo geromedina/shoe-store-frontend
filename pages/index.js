ad6a2faeec31191d097def37acbbf89a920fc450
geromedina <gerocode768@gmail.com>
2024-03-14 12:29:46 -0300

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,55 @@
+import HeroBanner from "@/components/HeroBanner";
+import ProductCard from "@/components/ProductCard";
+import Wrapper from "@/components/Wrapper";
+import { useState, useEffect } from "react";
+import { fetchDataFromApi } from "@/utils/api";
+
+export default function Home({ products }) {
+  // TODO ESTO LO HAGO ABAJO CON getStaticProps
+  // const [data, setData] = useState(null);
+
+  // useEffect(() => {
+  //   fetchProducts();
+  // }, []);
+
+  // const fetchProducts = async () => {
+  //   const { data } = await fetchDataFromApi("/api/products");
+  //   setData(data);
+  // };
+
+  return (
+    <main>
+      <HeroBanner />
+      <Wrapper>
+        {/* heading and paragraph start */}
+        <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
+          <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+            Cushioning for Your Miles
+          </div>
+          <div className="text-md md:text-xl">
+            A lightweight Nike ZoomX midsole is combined with increased stack
+            heights to help provide cushioning during extended stretches of
+            running.
+          </div>
+        </div>
+        {/* heading and paragraph end */}
+
+        {/* products grid start */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+          {products?.data?.map((product) => (
+            <ProductCard key={product?.id} data={product} />
+          ))}
+        </div>
+        {/* products grid end */}
+      </Wrapper>
+    </main>
+  );
+}
+
+export async function getStaticProps() {
+  const products = await fetchDataFromApi("/api/products?populate=*");
+
+  return {
+    props: { products: products },
+  };
+}
